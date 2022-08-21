7058a660905feaf08ff99d5bfc3f41ee891c2624
Petro-Hromiak <gromyakpetro@gmail.com>
2022-08-21 19:42:20 +0300

    HW 2 finished

--- HW2_loops_and_branches/main.js
+++ HW2_loops_and_branches/main.js
@@ -1,3 +1,31 @@
 let numberN = +prompt('Введіть початкове значення');
 let numberM = +prompt('Введіть кінцеве значення');
+let evenOrOdd = confirm('Чи бажаєте пропускати парні числа', true);
+let sum = 0;
 
+while (!Number.isInteger(numberN) || numberN == '') {
+    numberN = +prompt(`Початкове значення не є числом або не є цілим числом, будь-ласка введіть ціле число`);
+}
+while (!Number.isInteger(numberM) || numberM == '') {
+    numberM = +prompt(`Початкове значення не є числом або не є цілим числом, будь-ласка введіть ціле число`);
+}
+while (numberN >= numberM) {
+    numberN = +prompt(`Початкове значення є більшим або рівним кінцевому, введіть початкове значення`);
+    numberM = +prompt(`Кінцеве значення є меншим або рівним початковому, введіть кінцеве значення`);
+
+}
+for (let i = numberN; i <= numberM; i++) {
+    if (evenOrOdd && i % 2 == 0) {
+        continue
+    }
+    else {
+        sum += i;
+    }
+}
+if (evenOrOdd) {
+    document.writeln(`Сума непарних чисел в проміжку від ${numberN} до ${numberM} рівна ${sum}`)
+}
+else {
+    document.writeln(`Сума чисел в проміжку від ${numberN} до ${numberM} рівна ${sum}`)
+
+}
